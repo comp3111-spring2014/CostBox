c84a3777c4f9c9a46b11ed24984a53789781dad3
ruankd <ruankd@gmail.com>
2014-04-14 16:21:06 +0800

    update test cases

--- CostBoxTest/src/com/example/costbox/test/CostBoxTest.java
+++ CostBoxTest/src/com/example/costbox/test/CostBoxTest.java
@@ -1,20 +1,23 @@
 package com.example.costbox.test;
 
 import com.example.costbox.CostBox;
+import com.example.costbox.CostDetail;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class CostBoxTest extends ActivityInstrumentationTestCase2<CostBox> {
 
 	private CostBox mActivity; 
-	 Button buttonBook, buttonEnter; 
-	 EditText textCost; 
-	//delta - the maximum delta between expected and actual for which both numbers are still considered equal.
-	private static final double DELTA = 1e-15; // constant for comparing doubles values 
+	Button buttonBook, buttonFood, buttonTravel, buttonStudy, buttonEnter, buttonAdd, buttonReturn, buttonTrash;
+	EditText textCost; 
+	ListView myListView;
+	ImageView detailimage;
 
 	public CostBoxTest() { 
 		super(CostBox.class); 
@@ -28,12 +31,18 @@ public class CostBoxTest extends ActivityInstrumentationTestCase2<CostBox> {
 	mActivity = (CostBox) getActivity(); // get current activity 
 		
 	// link the objects with the activity objects 
-		buttonBook = (Button) mActivity 
-		.findViewById(com.example.costbox.R.id.book_select); 
-		buttonEnter = (Button) mActivity 
-		.findViewById(com.example.costbox.R.id.add_button); 
-		textCost = (EditText) mActivity 
-		.findViewById(com.example.costbox.R.id.myCost); 
+		buttonBook = (Button) mActivity.findViewById(com.example.costbox.R.id.book_select); 
+		buttonFood = (Button) mActivity.findViewById(com.example.costbox.R.id.food_select);
+		buttonTravel = (Button) mActivity.findViewById(com.example.costbox.R.id.travel_select);
+		buttonStudy = (Button) mActivity.findViewById(com.example.costbox.R.id.study_select);
+		buttonEnter = (Button) mActivity.findViewById(com.example.costbox.R.id.enter_select);
+		buttonAdd = (Button) mActivity.findViewById(com.example.costbox.R.id.add_button); 
+		textCost = (EditText) mActivity.findViewById(com.example.costbox.R.id.myCost); 
+		myListView = (ListView) mActivity.findViewById(com.example.costbox.R.id.myListView);
+		
+		detailimage = (ImageView) mActivity.findViewById(com.example.costbox.R.id.detailimage);
+		buttonReturn = (Button) mActivity.findViewById(com.example.costbox.R.id.returnbt);
+		buttonTrash = (Button) mActivity.findViewById(com.example.costbox.R.id.trashbt);
 	} 
 
 
@@ -51,24 +60,76 @@ public class CostBoxTest extends ActivityInstrumentationTestCase2<CostBox> {
 	} 
 
 	@SmallTest 
-	public void testKilosToPounds() { 
-
-		/* INTERACTIONS */ 
-		TouchUtils.clickView(this, buttonBook); // click the button buttonPounds 
-		TouchUtils.tapView(this, textCost); // tap the EditText textKilos 
-		sendKeys(123); // sent the number 1		
+	public void testBook() { 
+		TouchUtils.clickView(this, buttonBook);
+		TouchUtils.tapView(this, textCost);
+		sendKeys("1"); 	
+		sendKeys("0");
+		sendKeys("0");
 		
-		
-		TouchUtils.clickView(this, buttonEnter); // click the button buttonPounds 
-		/*CHECK THE RESULT*/  
-		
-
-		//JUnit Assert equals 
-
-		// message expected actual delta for comparing doubles 
-		//assertEquals("1 kilo is 2.20462262 pounds", 2.20462262, pounds, DELTA); 
+		TouchUtils.clickView(this, buttonAdd); // click the button buttonPounds  
 		assertTrue("hahaha",true);
 	} 
-
-
+	
+	@SmallTest 
+	public void testFood() { 
+		TouchUtils.clickView(this, buttonFood);
+		TouchUtils.tapView(this, textCost);
+		sendKeys("1"); 	
+		sendKeys("0");
+		sendKeys("0");
+		
+		TouchUtils.clickView(this, buttonAdd); // click the button buttonPounds  
+		assertTrue("hahaha",true);
+	} 
+	
+	@SmallTest 
+	public void testTravel() {  
+		TouchUtils.clickView(this, buttonTravel);
+		TouchUtils.tapView(this, textCost);
+		sendKeys("1"); 	
+		sendKeys("0");
+		sendKeys("0");
+		
+		TouchUtils.clickView(this, buttonAdd); // click the button buttonPounds  
+		assertTrue("hahaha",true);
+	} 
+	
+	@SmallTest 
+	public void testEnter() { 
+		TouchUtils.clickView(this, buttonEnter);
+		TouchUtils.tapView(this, textCost);
+		sendKeys("1"); 	
+		sendKeys("0");
+		sendKeys("0");
+		
+		TouchUtils.clickView(this, buttonAdd); // click the button buttonPounds  
+		assertTrue("hahaha",true);
+	} 
+	
+	@SmallTest 
+	public void testStudy() { 
+		TouchUtils.clickView(this, buttonStudy);
+		TouchUtils.tapView(this, textCost);
+		sendKeys("1"); 	
+		sendKeys("0");
+		sendKeys("0");
+		
+		TouchUtils.clickView(this, buttonAdd); // click the button buttonPounds  
+		assertTrue("hahaha",true);
+	}
+	
+//	public void testDetail(){
+//		TouchUtils.clickView(this, myListView.getChildAt(2));
+//		TouchUtils.clickView(this, buttonReturn);
+//		TouchUtils.clickView(this, myListView.getChildAt(2));
+//		TouchUtils.longClickView(this, detailimage);
+//		TouchUtils.clickView(this, buttonTrash);
+//		assertTrue("hahaha",true);
+//	}
+	
+	public void testLongClick(){
+		TouchUtils.longClickView(this, myListView.getChildAt(2));
+		assertTrue("hahaha",true);
+	}
 }
